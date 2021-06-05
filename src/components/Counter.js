312c048c91a92569b81c0a0cfb90859efec93c4c
Eka Fajhari Adwar <eka.adwar@gmail.com>
2021-06-05 23:56:04 +0700

    make summation and substraction data

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,16 +6,37 @@ class Counter extends React.Component {
     limit: "",
   };
 
+  subData = () => {
+    const count = this.state.result - 1;
+    if (count >= 0) {
+      this.setState({ limit: "" });
+      this.setState({ result: count });
+    } else {
+      this.setState({ limit: "This is minimum Number" });
+    }
+  };
+
+  sumData = () => {
+    const count = this.state.result + 1;
+    if (count <= 10) {
+      this.setState({ limit: "" });
+      this.setState({ result: count });
+    } else {
+      this.setState({ limit: "This is maximum Number" });
+    }
+  };
+
   render() {
     return (
       <React.Fragment>
         <div className="container">
           <h1>{this.props.counterTittle}</h1>
           <div className="counterBody">
-            <button>-</button>
+            <button onClick={this.subData}>-</button>
             <span>{this.state.result}</span>
-            <button>+</button>
+            <button onClick={this.sumData}>+</button>
           </div>
+          {this.state.limit}
         </div>
       </React.Fragment>
     );
