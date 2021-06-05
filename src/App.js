f385cea1defe8c27650c1fdbbd00d789aadcc610
Eka Fajhari Adwar <eka.adwar@gmail.com>
2021-06-05 22:45:00 +0700

    Start the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// ----- Form Login -------
+// import Form from "./components/FormLogin";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <h1>My First React</h1>
+//         <Form formTitle="This Title" />
+//       </React.Fragment>
+//     );
+//   }
+// }
+// ----- Akhir Form Login -------
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello Wolrd</h1>;
+  }
 }
 
 export default App;
---
+++ src/components/FormLogin.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+class FormLogin extends React.Component {
+  state = {
+    username: "",
+    password: "",
+    error: "",
+  };
+
+  componentDidUpdate() {
+    console.log(this.state);
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  inputChangePassword = (event) => {
+    this.setState({ password: event.target.value });
+    if (this.state.password.length < 6) {
+      this.setState({ error: "Password length must be greatter than 6" });
+    } else {
+      this.setState({ error: "" });
+    }
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit}>
+        {this.props.formTitle}
+        <div>Username</div>
+        <div>
+          <input onChange={(event) => this.setState({ username: event.target.value })} type="text" />
+        </div>
+        <div>Password</div>
+        <div>
+          <input onChange={this.inputChangePassword} type="text" />
+        </div>
+        <div>{this.state.error !== "" && this.state.error}</div>
+        <div>
+          <button>Login</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default FormLogin;
